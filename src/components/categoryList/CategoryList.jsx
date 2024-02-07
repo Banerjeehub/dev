32616e2bd27fba1bd918f0acefb7b17357489ea5
Padmanava_Banerjee <padmanavabanerjee2016@gmail.com>
2024-02-07 13:57:33 +0530

    bg

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 const getData = async () => {
-  const res = await fetch("https://dev-tau-vert.vercel.app/api/categories", {
+  const res = await fetch("https://dev-tau-vert.vercel.app/api/categorie", {
     cache: "no-store",
   });
 
