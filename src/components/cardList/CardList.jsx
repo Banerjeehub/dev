105be0511c916f5355177efb7ead91f887697555
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-03-16 09:45:27 +0530

    Update CardList.jsx

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -5,7 +5,7 @@ import Card from "../card/Card";
 
 const getData = async (page, cat) => {
   const res = await fetch(
-    `https://dev-tau-vert.vercel.app/api/posts?page=${page}&cat=${cat || ""}`,
+    `https://thought-canvas.vercel.app/api/posts?page=${page}&cat=${cat || ""}`,
     {
       cache: "no-store",
     }
